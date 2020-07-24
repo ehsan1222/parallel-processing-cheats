d1c3c856036912ec5c3afc90f0af1e3b082ce7a5
Ehsan Maddahi <s.e.maddahi.ir@gmail.com>
2020-07-24 11:24:03 +0430

    solve sum operation in dependent for loop

---
+++ open-mp-examples/example10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(void) {
+    int A[] = {1, 2, 3, 4};
+    int sum = 0;
+    int i;
+
+    // dependent for loop
+    #pragma omp parallel for num_threads(2) reduction(+: sum)
+    for(i = 0; i< 4; i++) {
+        sum += A[i];
+    }
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
