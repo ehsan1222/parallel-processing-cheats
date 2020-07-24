82b72b0d56a819ecb63c4a0874dbf84734373141
Ehsan Maddahi <s.e.maddahi.ir@gmail.com>
2020-07-24 11:09:19 +0430

    split for in two threads. <way 1>

---
+++ open-mp-examples/example8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+
+int main(void) {
+    int i;
+    
+    // split "for" in two threads
+    #pragma omp parallel num_threads(2)
+    {
+        #pragma omp for
+        for(i = 0; i < 4; i++){
+            printf("Loop: %d, Thread %d\n", i, omp_get_thread_num());
+        }
+    }
+
+    return 0;
+}
